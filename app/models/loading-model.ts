16f0994ce578e251a21c3b83edb5dba15393e258
George-Zzzz <15914683683@139.com>
2022-06-04 13:59:30 +0800

    init

--- app/models/loading-model.ts
+++ app/models/loading-model.ts
@@ -1,6 +1,17 @@
-import { atom } from 'recoil';
+import { atom, useRecoilValue, useSetRecoilState, selector } from "recoil";
 
 export const loadingState = atom({
-  key: 'loadingState',
+  key: "loadingState",
   default: false,
+});
+
+export const loading = useRecoilValue(loadingState);
+
+export const setLoading = useSetRecoilState(loadingState);
+
+export const isLoading = selector({
+  key: "isLoadingState",
+  get: ({ get }) => {
+    return loading;
+  },
 });
