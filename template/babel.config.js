a7e21b7403ecdd27c7e787c2cf55e6b66ab427c7
Simon <48503662+Simoon-F@users.noreply.github.com>
2022-07-11 09:44:14 +0800

    fixbug: fix alias configuration

--- template/babel.config.js
+++ template/babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          '@': './app',
+          '@config': './config',
+        },
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
 };
